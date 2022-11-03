5d4888f9de8653a7ee1d1885f37aaad877fa17b3
mao <1296193245@qq.com>
2022-11-03 21:24:05 +0800

    authority

---
+++ tools/tools-core/src/main/java/mao/tools_core/ToolsCoreApplication.java
@@ -0,0 +1,15 @@
+package mao.tools_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsCoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ToolsCoreApplication.class, args);
+    }
+
+}
