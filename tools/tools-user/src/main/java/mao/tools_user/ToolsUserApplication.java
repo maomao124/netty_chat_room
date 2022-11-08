055f548c2bd1761088476d4323c561cc972f15f3
mao <1296193245@qq.com>
2022-11-08 12:31:43 +0800

    authority

---
+++ tools/tools-user/src/main/java/mao/tools_user/ToolsUserApplication.java
@@ -0,0 +1,15 @@
+package mao.tools_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsUserApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ToolsUserApplication.class, args);
+    }
+
+}
