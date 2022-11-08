a05cf21de9b8de176309dac48dd9c99d230cbb76
mao <1296193245@qq.com>
2022-11-08 19:51:39 +0800

    authority

---
+++ apps/auth/auth-entity/src/main/java/mao/auth_entity/AuthEntityApplication.java
@@ -0,0 +1,15 @@
+package mao.auth_entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthEntityApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AuthEntityApplication.class, args);
+    }
+
+}
---
+++ apps/auth/auth-entity/src/test/java/mao/auth_entity/AuthEntityApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.auth_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthEntityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
