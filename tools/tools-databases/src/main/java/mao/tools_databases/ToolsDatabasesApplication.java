e58f2a317e865af6334dfeef4deba58ca1c48442
mao <1296193245@qq.com>
2022-11-08 13:44:36 +0800

    authority

--- tools/tools-databases/src/main/java/mao/tools_databases/ToolsDatabasesApplication.java
+++ tools/tools-databases/src/main/java/mao/tools_databases/ToolsDatabasesApplication.java
@@ -1,15 +1,15 @@
-package mao.tools_user;
+package mao.tools_databases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolsUserApplication
+public class ToolsDatabasesApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(ToolsUserApplication.class, args);
+        SpringApplication.run(ToolsDatabasesApplication.class, args);
     }
 
 }
