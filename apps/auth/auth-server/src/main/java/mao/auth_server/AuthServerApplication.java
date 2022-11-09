316ceb8faa2297df27681fe01dec7b78f4477fa1
mao <1296193245@qq.com>
2022-11-09 21:06:17 +0800

    authority

--- apps/auth/auth-server/src/main/java/mao/auth_server/AuthServerApplication.java
+++ apps/auth/auth-server/src/main/java/mao/auth_server/AuthServerApplication.java
@@ -28,6 +28,7 @@ public class AuthServerApplication
 
     public static void main(String[] args) throws UnknownHostException
     {
+        long start = System.currentTimeMillis();
         ConfigurableApplicationContext applicationContext = SpringApplication.run(AuthServerApplication.class, args);
         ConfigurableEnvironment environment = applicationContext.getEnvironment();
         String appName = environment.getProperty("spring.application.name");
@@ -37,6 +38,7 @@ public class AuthServerApplication
         //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
         //http://localhost:8080/doc.html
         log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html", appName, hostAddress, port);
+        log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
