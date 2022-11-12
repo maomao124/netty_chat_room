eb7675790a405aed787636c025c96282d598082f
mao <1296193245@qq.com>
2022-11-12 20:35:23 +0800

    authority

---
+++ apps/auth/auth-server/src/main/java/mao/auth_server/controller/auth/UserController.java
@@ -0,0 +1,21 @@
+package mao.auth_server.controller.auth;
+
+import mao.tools_core.base.BaseController;
+
+/**
+ * Project name(项目名称)：authority
+ * Package(包名): mao.auth_server.controller.auth
+ * Class(类名): UserController
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/11/12
+ * Time(创建时间)： 20:35
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class UserController extends BaseController
+{
+
+}
