1c33b5581ccc50035b05f27ef8ab698138c2a698
mao <1296193245@qq.com>
2022-12-30 20:14:08 +0800

    authority

---
+++ tools/tools-redis-cache/src/main/java/mao/tools_redis_cache/ToolsRedisCacheApplication.java
@@ -0,0 +1,15 @@
+package mao.tools_redis_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsRedisCacheApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ToolsRedisCacheApplication.class, args);
+    }
+
+}
