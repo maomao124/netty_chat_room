5125fdb03529714be4746a6613ee61d54a142d3e
mao <1296193245@qq.com>
2023-03-28 14:31:06 +0800

    群聊创建消息

---
+++ apps/chat-room/chat-room-common/src/main/java/mao/chat_room_common/message/GroupCreateRequestMessage.java
@@ -0,0 +1,43 @@
+package mao.chat_room_common.message;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.Set;
+
+/**
+ * Project name(项目名称)：netty_chat_room
+ * Package(包名): mao.chat_room_common.message
+ * Class(类名): GroupCreateRequestMessage
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/28
+ * Time(创建时间)： 14:22
+ * Version(版本): 1.0
+ * Description(描述)： 群聊创建请求消息
+ */
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ToString(callSuper = true)
+public class GroupCreateRequestMessage extends Message
+{
+
+    /**
+     * 组名称
+     */
+    private String groupName;
+
+    /**
+     * 群成员
+     */
+    private Set<String> members;
+
+    @Override
+    public int getMessageType()
+    {
+        return 0;
+    }
+}
---
+++ apps/chat-room/chat-room-common/src/main/java/mao/chat_room_common/message/GroupCreateResponseMessage.java
@@ -0,0 +1,83 @@
+package mao.chat_room_common.message;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Set;
+
+/**
+ * Project name(项目名称)：netty_chat_room
+ * Package(包名): mao.chat_room_common.message
+ * Class(类名): GroupCreateResponseMessage
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/28
+ * Time(创建时间)： 14:25
+ * Version(版本): 1.0
+ * Description(描述)： 群聊创建响应消息
+ */
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class GroupCreateResponseMessage extends AbstractResponseMessage
+{
+    /**
+     * 群成员
+     */
+    private Set<String> members;
+
+
+    @Override
+    public int getMessageType()
+    {
+        return 0;
+    }
+
+    public GroupCreateResponseMessage(Set<String> members)
+    {
+        this.members = members;
+    }
+
+    public GroupCreateResponseMessage(boolean success, String reason, Set<String> members)
+    {
+        super(success, reason);
+        this.members = members;
+    }
+
+    /**
+     * 成功
+     *
+     * @param members 成员
+     * @return {@link GroupCreateResponseMessage}
+     */
+    public static GroupCreateResponseMessage success(Set<String> members)
+    {
+        return new GroupCreateResponseMessage(true, null, members);
+    }
+
+    /**
+     * 失败
+     *
+     * @param reason 原因
+     * @return {@link GroupCreateResponseMessage}
+     */
+    public static GroupCreateResponseMessage fail(String reason)
+    {
+        return new GroupCreateResponseMessage(false, reason, null);
+    }
+
+    /**
+     * 失败
+     *
+     * @return {@link GroupCreateResponseMessage}
+     */
+    public static GroupCreateResponseMessage fail()
+    {
+        return new GroupCreateResponseMessage(false, "未知", null);
+    }
+}
