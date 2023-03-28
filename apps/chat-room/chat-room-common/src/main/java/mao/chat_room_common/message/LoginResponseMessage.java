fba8bdfa017335f2a118de5ce1efd0e3a5da1cac
mao <1296193245@qq.com>
2023-03-28 15:02:04 +0800

    用户登录消息

---
+++ apps/chat-room/chat-room-common/src/main/java/mao/chat_room_common/message/LoginResponseMessage.java
@@ -0,0 +1,72 @@
+package mao.chat_room_common.message;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Project name(项目名称)：netty_chat_room
+ * Package(包名): mao.chat_room_common.message
+ * Class(类名): LoginResponseMessage
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/28
+ * Time(创建时间)： 14:59
+ * Version(版本): 1.0
+ * Description(描述)： 登录响应消息
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class LoginResponseMessage extends AbstractResponseMessage
+{
+
+    @Override
+    public int getMessageType()
+    {
+        return 0;
+    }
+
+    public LoginResponseMessage()
+    {
+    }
+
+    public LoginResponseMessage(boolean success, String reason)
+    {
+        super(success, reason);
+    }
+
+    /**
+     * 成功
+     *
+     * @return {@link LoginResponseMessage}
+     */
+    public static LoginResponseMessage success()
+    {
+        return new LoginResponseMessage(true, null);
+    }
+
+
+    /**
+     * 失败
+     *
+     * @return {@link LoginResponseMessage}
+     */
+    public static LoginResponseMessage fail()
+    {
+        return new LoginResponseMessage(false, "未知");
+    }
+
+    /**
+     * 失败
+     *
+     * @param reason 原因
+     * @return {@link LoginResponseMessage}
+     */
+    public static LoginResponseMessage fail(String reason)
+    {
+        return new LoginResponseMessage(false, reason);
+    }
+}
