64a6627872bd5e2cfb11f6fdde315d4701d18d12
mao <1296193245@qq.com>
2023-03-29 21:35:39 +0800

    添加业务代码

---
+++ apps/chat-room/chat-room-netty-server/src/main/java/mao/chat_room_netty_server/session/GroupSessionMemoryImpl.java
@@ -0,0 +1,114 @@
+package mao.chat_room_netty_server.session;
+
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+/**
+ * Project name(项目名称)：netty_chat_room
+ * Package(包名): mao.chat_room_netty_server.session
+ * Class(类名): GroupSessionMemoryImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/29
+ * Time(创建时间)： 21:16
+ * Version(版本): 1.0
+ * Description(描述)： 聊天组会话管理接口实现类
+ */
+
+@Slf4j
+@Service
+public class GroupSessionMemoryImpl implements GroupSession
+{
+
+    private final Session session;
+
+    @Autowired
+    public GroupSessionMemoryImpl(Session session)
+    {
+        this.session = session;
+    }
+
+    private final Map<String, Group> groupMap = new ConcurrentHashMap<>();
+
+
+    @Override
+    public Group createGroup(String name, Set<String> members)
+    {
+        log.debug("创建群聊：" + name + "，成员：" + members);
+        Group group = new Group(name, members);
+        return groupMap.putIfAbsent(name, group);
+    }
+
+    @Override
+    public Group joinMember(String name, String member)
+    {
+        log.debug("成员：" + member + ",加入群聊：" + name);
+        return groupMap.computeIfPresent(name, new BiFunction<String, Group, Group>()
+        {
+            @Override
+            public Group apply(String s, Group group)
+            {
+                //添加
+                group.getMembers().add(member);
+                return group;
+            }
+        });
+    }
+
+    @Override
+    public Group removeMember(String name, String member)
+    {
+        log.debug("成员：" + member + ",退出群聊：" + name);
+        return groupMap.computeIfPresent(name, new BiFunction<String, Group, Group>()
+        {
+            @Override
+            public Group apply(String s, Group group)
+            {
+                //移除群成员
+                group.getMembers().remove(member);
+                return group;
+            }
+        });
+    }
+
+    @Override
+    public Group removeGroup(String name)
+    {
+        log.debug("移除群聊：" + name);
+        return groupMap.remove(name);
+    }
+
+    @Override
+    public Set<String> getMembers(String name)
+    {
+        log.debug("得到群聊：" + name + " 的所有群成员");
+        Set<String> members = groupMap.get(name).getMembers();
+        if (members == null)
+        {
+            return Group.EMPTY_GROUP.getMembers();
+        }
+        log.debug("群成员：" + members);
+        return members;
+    }
+
+    @Override
+    public List<Channel> getMembersChannel(String name)
+    {
+        return getMembers(name).stream()
+                .map(session::getChannel)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+}
---
+++ apps/chat-room/chat-room-netty-server/src/main/java/mao/chat_room_netty_server/session/SessionMemoryImpl.java
@@ -0,0 +1,85 @@
+package mao.chat_room_netty_server.session;
+
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Project name(项目名称)：netty_chat_room
+ * Package(包名): mao.chat_room_netty_server.session
+ * Class(类名): SessionMemoryImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/29
+ * Time(创建时间)： 21:01
+ * Version(版本): 1.0
+ * Description(描述)： 会话管理接口实现类
+ */
+
+@Slf4j
+@Service
+public class SessionMemoryImpl implements Session
+{
+
+    /**
+     * 用户名通道映射
+     */
+    private final Map<String, Channel> usernameChannelMap = new ConcurrentHashMap<>();
+
+    /**
+     * channel用户名映射
+     */
+    private final Map<Channel, String> channelUsernameMap = new ConcurrentHashMap<>();
+
+    /**
+     * 通道属性映射
+     */
+    private final Map<Channel, Map<String, Object>> channelAttributesMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void bind(Channel channel, String username)
+    {
+        log.debug("用户：" + username + "绑定会话");
+        usernameChannelMap.put(username, channel);
+        channelUsernameMap.put(channel, username);
+        channelAttributesMap.put(channel, new ConcurrentHashMap<>());
+    }
+
+    @Override
+    public void unbind(Channel channel)
+    {
+        //得到用户名
+        String username = channelUsernameMap.remove(channel);
+        log.debug("用户：" + username + "绑定会话");
+        //移除
+        usernameChannelMap.remove(username);
+        channelUsernameMap.remove(channel);
+        channelAttributesMap.remove(channel);
+    }
+
+    @Override
+    public Object getAttribute(Channel channel, String name)
+    {
+        log.debug("得到" + name + "的属性值");
+        return channelAttributesMap.get(channel).get(name);
+    }
+
+    @Override
+    public void setAttribute(Channel channel, String name, Object value)
+    {
+        log.debug("设置属性" + name + "--->" + value);
+        Map<String, Object> map = channelAttributesMap.get(channel);
+        map.put(name, value);
+    }
+
+    @Override
+    public Channel getChannel(String username)
+    {
+        log.debug("根据用户名获取channel：" + username);
+        return usernameChannelMap.get(username);
+    }
+}
