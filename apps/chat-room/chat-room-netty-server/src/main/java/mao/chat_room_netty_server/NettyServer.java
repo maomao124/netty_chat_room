a2b928fb9d77be10c12daaa52e177a3142966d34
mao <1296193245@qq.com>
2023-03-30 15:58:50 +0800

    添加业务代码

--- apps/chat-room/chat-room-netty-server/src/main/java/mao/chat_room_netty_server/NettyServer.java
+++ apps/chat-room/chat-room-netty-server/src/main/java/mao/chat_room_netty_server/NettyServer.java
@@ -1,6 +1,14 @@
 package mao.chat_room_netty_server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import lombok.extern.slf4j.Slf4j;
 import mao.chat_room_common.protocol.ProcotolFrameDecoder;
 import mao.chat_room_netty_server.handler.*;
@@ -11,6 +19,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.concurrent.locks.LockSupport;
 
 
 /**
@@ -67,13 +76,97 @@ public class NettyServer implements CommandLineRunner
     @Resource
     private QuitHandler quitHandler;
 
+    /**
+     * 运行,禁止长时间阻塞此线程
+     *
+     * @param args 参数
+     * @throws Exception 异常
+     */
     @Override
     public void run(String... args) throws Exception
     {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
 
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+
+        try
+        {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            Channel channel = serverBootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioServerSocketChannel>()
+                    {
+                        @Override
+                        protected void initChannel(NioServerSocketChannel ch) throws Exception
+                        {
+                            ch.pipeline().addLast(LOGGING_HANDLER)
+                                    .addLast(procotolFrameDecoder)
+                                    .addLast(serverMessageCodecSharable)
+                                    .addLast(chatRequestMessageHandler)
+                                    .addLast(groupChatRequestMessageHandler)
+                                    .addLast(groupCreateRequestMessageHandler)
+                                    .addLast(groupJoinRequestMessageHandler)
+                                    .addLast(groupMembersRequestMessageHandler)
+                                    .addLast(groupQuitRequestMessageHandler)
+                                    .addLast(loginRequestMessageHandler)
+                                    .addLast(registerRequestMessageHandler)
+                                    .addLast(quitHandler);
+                        }
+                    }).bind(serverConfig.getServerPort()).sync().channel();
+            log.info("Netty服务器启动成功");
+            channel.closeFuture().addListener(new GenericFutureListener<Future<? super Void>>()
+            {
+                @Override
+                public void operationComplete(Future<? super Void> future) throws Exception
+                {
+                    log.info("正在关闭服务器...");
+                    close(boss, worker);
+                }
+            });
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    log.info("正在关闭服务器...");
+                    close(boss, worker);
+                }
+            }));
+        }
+        catch (Exception e)
+        {
+            log.info("Netty服务器启动失败");
+            throw new RuntimeException(e);
+        }
     }
+
+    /**
+     * 关闭
+     *
+     * @param boss   NioEventLoopGroup
+     * @param worker NioEventLoopGroup
+     */
+    private void close(NioEventLoopGroup boss, NioEventLoopGroup worker)
+    {
+        try
+        {
+            boss.shutdownGracefully();
+        }
+        catch (Exception ignored)
+        {
+
+        }
+        try
+        {
+            worker.shutdownGracefully();
+        }
+        catch (Exception ignored)
+        {
+
+        }
+    }
+
 
     @PostConstruct
     public void init()
