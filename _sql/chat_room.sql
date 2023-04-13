3ec24da8be87eddb4dac8e2e93c8cf30b2407dde
mao <1296193245@qq.com>
2023-04-13 13:20:06 +0800

    添加业务代码

--- _sql/chat_room.sql
+++ _sql/chat_room.sql
@@ -5,12 +5,14 @@
 DROP TABLE IF EXISTS `auth_user`;
 CREATE TABLE `auth_user`
 (
-    `id`              bigint(20) NOT NULL COMMENT 'ID',
-    `username`        varchar(30) NOT NULL COMMENT '账号',
-    `status`          bit(1)               DEFAULT b'0' COMMENT '启用状态 1启用 0禁用',
-    `password`        varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
-    `last_login_time` datetime             DEFAULT NULL COMMENT '最后登录时间',
-    `register_time`   datetime             DEFAULT NULL COMMENT '注册时间',
+    `id`                       bigint(20) NOT NULL COMMENT 'ID',
+    `username`                 varchar(30) NOT NULL COMMENT '账号',
+    `status`                   bit(1)               DEFAULT b'0' COMMENT '启用状态 1启用 0禁用',
+    `password`                 varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
+    `last_login_time`          datetime             DEFAULT NULL COMMENT '最后登录时间',
+    `register_time`            datetime             DEFAULT NULL COMMENT '注册时间',
+    `password_error_last_time` datetime             DEFAULT NULL COMMENT '最后一次输错密码时间',
+    `password_error_num`       int(11) DEFAULT '0' COMMENT '密码错误次数',
     PRIMARY KEY (`id`) USING BTREE,
     UNIQUE KEY `UN_username` (`username`) USING BTREE
 ) ENGINE = InnoDB
--- apps/chat-room/chat-room-server-api/src/main/java/mao/chat_room_server_api/dto/UserDTO.java
+++ apps/chat-room/chat-room-server-api/src/main/java/mao/chat_room_server_api/dto/UserDTO.java
@@ -1,7 +1,7 @@
-package mao.chat_room_server_api.entity;
+package mao.chat_room_server_api.dto;
 
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import lombok.experimental.Accessors;
@@ -13,39 +13,34 @@ import static com.baomidou.mybatisplus.annotation.SqlCondition.LIKE;
 
 /**
  * Project name(项目名称)：netty_chat_room
- * Package(包名): mao.chat_room_server_api.entity
- * Class(类名): User
+ * Package(包名): mao.chat_room_server_api.dto
+ * Class(类名): UserDTO
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
- * Date(创建日期)： 2023/3/28
- * Time(创建时间)： 21:43
+ * Date(创建日期)： 2023/4/13
+ * Time(创建时间)： 13:15
  * Version(版本): 1.0
- * Description(描述)： 用户实体类
+ * Description(描述)： 无
  */
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode
+@ApiModel(value = "用户DTO")
 @Accessors(chain = true)
-@TableName("auth_user")
-public class User implements Serializable
+public class UserDTO implements Serializable
 {
-
     private static final long serialVersionUID = 1L;
-
-    /**
-     * id
-     */
-    @TableField("id")
-    private Long id;
 
     /**
      * 用户名
      */
     @TableField("username")
+    @ApiModelProperty(value = "用户名", notes = "用户名")
     private String username;
 
 
@@ -53,36 +48,35 @@ public class User implements Serializable
      * 启用状态 1启用 0禁用
      */
     @TableField("status")
+    @ApiModelProperty(value = "启用状态 1启用 0禁用", notes = "启用状态 1启用 0禁用")
     private Boolean status;
 
-
-    /**
-     * 密码
-     */
-    @TableField(value = "password", condition = LIKE)
-    private String password;
 
     /**
      * 最后登录时间
      */
     @TableField("last_login_time")
+    @ApiModelProperty(value = "最后登录时间", notes = "最后登录时间")
     private LocalDateTime lastLoginTime;
 
     /**
      * 注册时间
      */
     @TableField("register_time")
+    @ApiModelProperty(value = "注册时间", notes = "注册时间")
     private LocalDateTime registerTime;
 
     /**
      * 最后一次输错密码时间
      */
     @TableField("password_error_last_time")
+    @ApiModelProperty(value = "最后一次输错密码时间", notes = "最后一次输错密码时间")
     private LocalDateTime passwordErrorLastTime;
 
     /**
      * 密码错误次数
      */
     @TableField("password_error_num")
+    @ApiModelProperty(value = "密码错误次数", notes = "密码错误次数")
     private Integer passwordErrorNum;
 }
